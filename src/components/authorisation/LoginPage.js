ce665a00bcb0ceccd53e60105b73cb58c740dfc1
Greg Shanks <greg.shanks@gmail.com>
2020-02-25 05:41:38 +0000

    Added onChange functions for updating username and password

--- src/components/authorisation/LoginPage.js
+++ src/components/authorisation/LoginPage.js
@@ -9,13 +9,25 @@ class LoginPage extends Component {
     }
   }
 
+  handleUsername(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePassword(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
   render() {
     <div>
       <form action="">
       <label >Username: </label>
-      <input type="text" value={this.state.username}/>
+      <input type="text" value={this.state.username} onChange={this.handleUsername}/>
       <label >Password: </label>
-      <input type="text" value={this.state.password}/>
+      <input type="text" value={this.state.password} onChange={this.handlePassword}/>
       <input type="submit" value="Submit"/>
       </form>
     </div>
