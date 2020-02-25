a9f73d4ace2dbeedc2e2ace5f0cea6c8c0ae1c21
Greg Shanks <greg.shanks@gmail.com>
2020-02-25 06:59:04 +0000

    Minor fixes

--- src/components/accounts/UserLoginForm.js
+++ src/components/accounts/UserLoginForm.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 
 class UserLoginForm extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       username: '',
       password: '',
