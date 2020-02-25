64a86124e7978a4751bbcf6f143cbce66b9e1444
Greg Shanks <greg.shanks@gmail.com>
2020-02-25 05:28:42 +0000

    Setup constructor for LoginPage

---
+++ src/components/authorisation/LoginPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class LoginPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+}
+
+export default LoginPage;
