f87bc6a9cc6a037993fcb79cbde8f4a903510016
Greg Shanks <greg.shanks@gmail.com>
2020-02-25 06:40:15 +0000

    Created constructor for UserCreateForm

--- src/components/accounts/UserCreateForm.js
+++ src/components/accounts/UserCreateForm.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
 
 class UserCreateForm extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      username: "",
+      password: "",
+      profilePicture: "",
+      mood: ""
+    }
+  }
 }
 
 export default UserCreateForm;
